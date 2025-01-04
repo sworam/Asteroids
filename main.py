8c9688ef22c9403e3a5c301295390ff9973b6d04
Marlo Swora <marlo.swora@mail.de>
2025-01-04 16:54:22 +0100

    add deltatime and 60 FPS restriction

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
                 return
         screen.fill(0)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
